0dce80967a4e49a71a5d6ec2128765a0dd364b7b
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-06-27 15:20:05 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+Public class Even{
+  Static int test(int num){
+    if(num % 2 == 0)
+      return 2;
+    else
+      return 1;
+  }
+  Public static void main(){
+    int number;
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter the number");
+    number = s.nextInt();
+    System.out.println("Result " +test(number));
+  }
+}
