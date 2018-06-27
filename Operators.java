51beb2c12f549308f1108e01ac87274b0a8cd539
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-06-27 14:16:40 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+      int a = 5,b = 10;
+      System.out.println(+(a+b)"\n" +(a-b) "\n" +(a*b)"\n" +(b/a) "\n" +(b%a) "/n" +(b++) "\n" +(b--) "\n" +(a*b) "\n" +(a|b) "\n" +(a^b) "\n" +(~a) "\n" +(a<<2) "\n" +(a>>2) "\n" +(a>>>2) "\n" +(a&&b) "\n" +(a||b) "\n" +(!(a&&b)) );
+	}
+}
