27a50b995061173c4c9ab2b161e023dac15128f8
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-07-05 15:56:30 +0530

    Create Password_strength.java

---
+++ Password_strength.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		string passw;
+		string normalchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345467890"
+		Scanner s = new Scanner(System.in);
+		passw = s.next();
+		int lower = 0,upper = 0,digit = 0,special = 0;
+		count = passw.length();
+		for(int i =0;i<n;i++){
+			if(islower(passw[i])
+				lower = 1;
+			if(isupper(passw[i])
+				upper = 1;
+			if(isdigit(passw[i])
+				digit = 1;
+			size_t special1 = input.find_first_not_of(normalChars);
+        	if (special != paasw::npos)
+            	special = 0;
+		}
+		
+	}
+}
