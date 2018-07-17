9b5982067353649efac0d5d38e312c23747aa125
Jothi Priya Sundaravdivel <34646744+priya004@users.noreply.github.com>
2018-07-17 14:44:14 +0530

    Create String_compare_equals.java

---
+++ String_compare_equals.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sample {
+	public static void main(String args[]){
+		String s1 = "Cristina";
+		String s2 = "Cristina";
+		String s3 = "Ronaldo";
+		System.out.println(s1.equals(s2));
+		System.out.println(s2.equals(s3));
+	}
+}
